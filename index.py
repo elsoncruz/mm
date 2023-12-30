69ade4be54991afe64c28fbccd4394afdd8ffacf
elsoncruz <128441481+elsoncruz@users.noreply.github.com>
2023-12-30 14:18:20 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,18 +1,9 @@
-#from langchain.document_loaders import PyPDFLoader
-import streamlit as st
-#loader= PyPDFLoader("python.pdf")
-#pages_content=loader.load_and_split()
-#print(len(pages_content),pages_content)
 
-pass={
-    "authorization":st.secrets["api_key"],
-    "content-type":"application/jason"
-}
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
-embeddings=OpenAIEmbeddings(api_key=pass)
+embeddings=OpenAIEmbeddings(api_key="")
 #db=FAISS.from_documents(pages_content,embeddings)
 
 #db.save_local("faiss_index")
@@ -22,7 +13,7 @@ new_db=FAISS.load_local("faiss_index",embeddings)
 
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
-llm= ChatOpenAI(api_key=pass)
+llm= ChatOpenAI(api_key="")
 
 qa_chain=RetrievalQA.from_chain_type(llm,retriever=new_db.as_retriever())
 def ask(user_query):
