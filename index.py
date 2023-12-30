c018914ceff49b9468fdf2251248c298f70c087c
elsoncruz <128441481+elsoncruz@users.noreply.github.com>
2023-12-30 12:13:33 +0530

    Update index.py

--- index.py
+++ index.py
@@ -7,7 +7,7 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
-embeddings=OpenAIEmbeddings(api_key="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")
+embeddings=OpenAIEmbeddings(api_key="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")
 #db=FAISS.from_documents(pages_content,embeddings)
 
 #db.save_local("faiss_index")
@@ -17,7 +17,7 @@ new_db=FAISS.load_local("faiss_index",embeddings)
 
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
-llm= ChatOpenAI(api_key="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")
+llm= ChatOpenAI(api_key="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")
 
 qa_chain=RetrievalQA.from_chain_type(llm,retriever=new_db.as_retriever())
 def ask(user_query):
